4b3f29ba05972217cedee31888596bbeceeef4f4
Alpha-Nero <114919406+Alpha-Nero@users.noreply.github.com>
2022-10-04 16:32:38 +0400

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
 
 const host = 0.0.0.0;
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, host, () => {
     console.log(`App is running on port ${PORT}`)
 })
